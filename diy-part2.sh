9bf66a6b1b8f920311edcc870a0759a9beb6901b
anai1943 <anai1943@163.com>
2021-06-11 10:49:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,18 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+# Clone community packages to package/community
+mkdir package/community
+pushd package/communit
+
+# passwall 
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+
+# 文件管理 无法编译安装 进入github下载ipk手动安装
+#git clone https://github.com/lyin888/luci-app-filebrowser.git
+git clone --depth=1 https://github.com/MonwF/luci-app-filebrowser.git
+
+#应用过滤 https://github.com/destan19/OpenAppFilter
+git clone --depth=1 https://github.com/destan19/OpenAppFilter
