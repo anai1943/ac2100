d81ee087c2515d5d7a94664d7d7e02b3dc12dd77
anai1943 <anai1943@163.com>
2021-06-11 22:09:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,14 +12,14 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_generate
 
 
 
 # passwall 
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall
 
 #应用过滤 https://github.com/destan19/OpenAppFilter
-git clone --depth=1 https://github.com/destan19/OpenAppFilter
+git clone https://github.com/destan19/OpenAppFilter
 
 
