a078fcbe8aa6cee263a1fe8dc09b8ccbc0584762
anai1943 <anai1943@163.com>
2021-06-11 12:07:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,9 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# Clone community packages to package/community
-mkdir package/community
-pushd package/communit
+
 
 # passwall 
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
@@ -24,4 +22,4 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 #应用过滤 https://github.com/destan19/OpenAppFilter
 git clone --depth=1 https://github.com/destan19/OpenAppFilter
 
-popd
+
